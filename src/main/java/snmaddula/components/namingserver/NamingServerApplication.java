0ab45452145f91dbbea9f3b4ee60143d5f2ee490
snmaddula <sn_maddula@outlook.com>
2018-11-29 03:52:46 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/components/namingserver/NamingServerApplication.java
@@ -0,0 +1,12 @@
+package snmaddula.components.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+}
